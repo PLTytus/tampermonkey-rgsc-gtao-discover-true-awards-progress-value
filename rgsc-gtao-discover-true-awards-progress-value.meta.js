abf0aa2d5a0c2f33f4664c90cd532415e544fab3
PLTytus <rafal.tytus98@gmail.com>
2024-07-02 20:41:17 +0200

    1.1.3 - support viewing others

--- rgsc-gtao-discover-true-awards-progress-value.meta.js
+++ rgsc-gtao-discover-true-awards-progress-value.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name            RGSC Discover True Awards Progress Value
+// @name            RGSC-GTAO Discover True Awards Progress Value
 // @author          PLTytus
-// @version         1.1.2
+// @version         1.1.3
 // @namespace       http://gtaweb.eu/tampermonkey
 // @downloadURL     https://github.com/PLTytus/tampermonkey-rgsc-gtao-discover-true-awards-progress-value/raw/master/rgsc-gtao-discover-true-awards-progress-value.user.js
 // @updateURL       https://github.com/PLTytus/tampermonkey-rgsc-gtao-discover-true-awards-progress-value/raw/master/rgsc-gtao-discover-true-awards-progress-value.meta.js
 // @match           https://socialclub.rockstargames.com/games/gtav/*/career/awards
 // @match           https://socialclub.rockstargames.com/games/gtav/*/career/awards/*
+// @match           https://socialclub.rockstargames.com/member/*/games/gtav/*/career/awards
+// @match           https://socialclub.rockstargames.com/member/*/games/gtav/*/career/awards/*
 // ==/UserScript==
