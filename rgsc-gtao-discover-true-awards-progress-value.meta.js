4c77b8ec22835a0837b4edea816b54ae5d106e89
PLTytus <rafal.tytus98@gmail.com>
2024-07-02 10:20:23 +0200

    fix urls

--- rgsc-gtao-discover-true-awards-progress-value.meta.js
+++ rgsc-gtao-discover-true-awards-progress-value.meta.js
@@ -3,8 +3,8 @@
 // @author          PLTytus
 // @version         1.1.0
 // @namespace       http://gtaweb.eu/tampermonkey
-// @downloadURL     https://github.com/PLTytus/tampermonkey-rgsc-discover-true-awards-progress-value/raw/master/rgsc-discover-true-awards-progress-value.user.js
-// @updateURL       https://github.com/PLTytus/tampermonkey-rgsc-discover-true-awards-progress-value/raw/master/rgsc-discover-true-awards-progress-value.meta.js
+// @downloadURL     https://github.com/PLTytus/tampermonkey-rgsc-discover-true-awards-progress-value/raw/master/rgsc-gtao-discover-true-awards-progress-value.user.js
+// @updateURL       https://github.com/PLTytus/tampermonkey-rgsc-discover-true-awards-progress-value/raw/master/rgsc-gtao-discover-true-awards-progress-value.meta.js
 // @match           https://socialclub.rockstargames.com/games/gtav/pc/career/awards
 // @match           https://socialclub.rockstargames.com/games/gtav/pc/career/awards/*
 // ==/UserScript==
